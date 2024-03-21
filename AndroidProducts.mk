721b32fbffe26d1f1055e587e04f3a598a26e2a7
Tactus <tactus1@proton.me>
2024-03-21 12:36:19 +0000

    tequila_bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_raphael.mk
+    $(LOCAL_DIR)/tequila_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_raphael-user \
-    derp_raphael-userdebug \
-    derp_raphael-eng
+    tequila_raphael-user \
+    tequila_raphael-userdebug \
+    tequila_raphael-eng
---
+++ tequila_raphael.mk
@@ -0,0 +1,49 @@
+# Copyright (C) 2023 DerpFest
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common TequilaOS stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
+
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Disable/Enable Blur Support, default is false
+TARGET_ENABLE_BLUR := true
+
+# Derp
+DERP_BUILDTYPE := Official
+
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Quick Tap Feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := tequila_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 9T Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="raphael"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
